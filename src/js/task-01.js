d8323ea6b109bbccc3a75a500d5632faaaab7cdf
IschukM <dzubak@ukr.net>
2022-11-26 22:12:58 +0200

    Update task-01.js

---
+++ src/js/task-01.js
@@ -0,0 +1,13 @@
+const itemEl = document.querySelectorAll(".item");
+console.log("Number of categories", itemEl.length);
+
+// const titleEl = document.querySelectorAll(".item h2");
+// console.log("Category:", titleEl[0].textContent);
+
+// const quantity = ItemEl[0].lastElementChild.children.length;
+
+// console.log(bb);
+console.log(itemEl);
+const listItems = itemEl.map((itm) => {
+  //   titleEl = itm.firstChild.textContent;
+});
