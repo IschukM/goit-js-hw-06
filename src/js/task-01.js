be167c43d2701679d488740dc64bdbbf65f9f10e
IschukM <dzubak@ukr.net>
2022-12-02 20:00:07 +0200

    Update task-01.js

--- src/js/task-01.js
+++ src/js/task-01.js
@@ -2,7 +2,7 @@ const itemEl = document.querySelectorAll(".item");
 console.log("Number of categories", itemEl.length);
 
 const report = itemEl.forEach((rep) => {
-  const titleEl = planet.querySelector("h2");
+  const titleEl = rep.querySelector("h2");
   console.log("Category:", titleEl.textContent);
   console.log("Elements", rep.lastElementChild.children.length);
 });
