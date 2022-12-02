6992adbd4f2f455a3d589f614af33223e667a195
IschukM <dzubak@ukr.net>
2022-12-02 20:11:09 +0200

    Update task-06.js

--- src/js/task-06.js
+++ src/js/task-06.js
@@ -1,7 +1,13 @@
 const validationInput = document.querySelector("#validation-input");
+console.log(validationInput.dataset.length);
+console.log(validationInput.value.length);
+
 validationInput.addEventListener("blur", (event) => {
-  console.log(event);
-  //   if ((validationInput.value.length = 6)) {
-  //     validationInput.classList.add("#validation-input.valid");
-  //   }
+  let ddd = event.currentTarget.value.length;
+  console.log(ddd);
+  if (ddd === validationInput.dataset.length) {
+    validationInput.classList.add("validation-input.valid");
+    console.log(validationInput.value);
+  }
 });
+// console.log(validationInput.value);
